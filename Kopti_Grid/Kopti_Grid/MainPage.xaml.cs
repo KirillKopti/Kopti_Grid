ab2057908ccff2c3ff715c01fe0344d4b20245ca
Opilane TTHK <opilane@tthk.ee>
2020-11-09 17:30:42 +0200

    Add project files.

---
+++ Kopti_Grid/Kopti_Grid/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Kopti_Grid
+{
+    public partial class MainPage : ContentPage
+    {
+        Picker picker;
+        Editor editor;
+        public MainPage()
+        {
+            Grid gr = new Grid
+            {
+                RowDefinitions =
+                {
+                    new RowDefinition{Height=new GridLength(1, GridUnitType.Star)},
+                    new RowDefinition{Height=new GridLength(1, GridUnitType.Star)},
+                    new RowDefinition{Height=new GridLength(1, GridUnitType.Star)},
+                },
+                ColumnDefinitions =
+                {
+                    new ColumnDefinition{Width=new GridLength(1, GridUnitType.Star)},
+                    new ColumnDefinition{Width=new GridLength(1, GridUnitType.Star)},
+                }
+            };
+            picker = new Picker
+            {
+                Title = "Keeled"
+            };
+            picker.Items.Add("C#");
+            picker.Items.Add("Python");
+            picker.Items.Add("C++");
+            picker.Items.Add("VisualBasic");
+            picker.Items.Add("Java");
+            gr.Children.Add(picker, 0, 0);
+
+            editor = new Editor { Placeholder = "Vali keel \n nimikirjast" };
+            gr.Children.Add(editor, 1, 0);
+
+            Content = gr;
+        }
+        private void Picker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            editor.Text = "Oli valitud: " + picker.Items[picker.SelectedIndex];
+        }
+
+    }
+}
